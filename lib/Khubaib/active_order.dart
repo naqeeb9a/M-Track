0b9289da1ca606be15622d5b75eb47a217268a6b
khubaibbinasad <khubaibbinasad786@gmail.com>
2022-02-18 11:27:23 +0500

    Khubaib - Active order

--- lib/Khubaib/active_order.dart
+++ lib/Khubaib/active_order.dart
@@ -4,7 +4,7 @@ import 'package:courierapp/utils/dynamic_sizes.dart';
 import 'package:flutter/material.dart';
 
 class ActiveOrder extends StatefulWidget {
-  const ActiveOrder({ Key? key }) : super(key: key);
+  const ActiveOrder({Key? key}) : super(key: key);
 
   @override
   _ActiveOrderState createState() => _ActiveOrderState();
@@ -14,7 +14,6 @@ class _ActiveOrderState extends State<ActiveOrder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Container(
         width: CustomSizes().dynamicWidth(context, 1),
         height: CustomSizes().dynamicHeight(context, 1),
@@ -26,18 +25,39 @@ class _ActiveOrderState extends State<ActiveOrder> {
             Container(
               width: CustomSizes().dynamicWidth(context, 0.85),
               height: CustomSizes().dynamicHeight(context, .15),
-              // color: CustomColors.customWhite,
               decoration: BoxDecoration(
-                color:CustomColors.customWhite,
-                borderRadius: BorderRadius.circular(CustomSizes().dynamicWidth(context, 0.05),)
+                  color: CustomColors.customWhite,
+                  borderRadius: BorderRadius.circular(
+                    CustomSizes().dynamicWidth(context, 0.05),
+                  )),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: CustomSizes().dynamicWidth(context, 0.3),
+                    height: CustomSizes().dynamicHeight(context, 0.1),
+                    child: Image.network(
+                      "https://www.pngkit.com/png/full/72-724560_png-file-tracking-parcel-png.png",
+                      color: CustomColors.customYellow,
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        text(context, "Send Package", 0.045,CustomColors.customBlack,bold:true),
+                        CustomSizes().heightBox(context,0.02),
+                        text(context, "Deliver or recieve items such as gifts ,documents,keys", 0.035, CustomColors.customGrey),
+                      ],
+                    ),
+                  )
+                ],
               ),
-
             ),
-            CustomSizes().heightBox(context,0.05),
+            CustomSizes().heightBox(context, 0.065),
             Container(
               width: CustomSizes().dynamicWidth(context, 0.85),
               height: CustomSizes().dynamicHeight(context, .15),
-              // color: CustomColors.customWhite,
               decoration: BoxDecoration(
                   color: CustomColors.customWhite,
                   borderRadius: BorderRadius.circular(
