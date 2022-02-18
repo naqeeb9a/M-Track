819d757d99e9d6fcb94501bd17096eb51907d273
khubaibbinasad <khubaibbinasad786@gmail.com>
2022-02-18 17:29:45 +0500

    khubaib -active order ,completed

--- lib/Khubaib/active_order.dart
+++ lib/Khubaib/active_order.dart
@@ -14,59 +14,70 @@ class _ActiveOrderState extends State<ActiveOrder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: CustomSizes().dynamicWidth(context, 1),
-        height: CustomSizes().dynamicHeight(context, 1),
-        color: CustomColors.customGrey.withOpacity(0.2),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: CustomSizes().dynamicWidth(context, 0.85),
-              height: CustomSizes().dynamicHeight(context, .15),
-              decoration: BoxDecoration(
-                  color: CustomColors.customWhite,
-                  borderRadius: BorderRadius.circular(
-                    CustomSizes().dynamicWidth(context, 0.05),
-                  )),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: CustomSizes().dynamicWidth(context, 0.3),
-                    height: CustomSizes().dynamicHeight(context, 0.1),
-                    child: Image.network(
-                      "https://www.pngkit.com/png/full/72-724560_png-file-tracking-parcel-png.png",
-                      color: CustomColors.customYellow,
-                    ),
-                  ),
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        text(context, "Send Package", 0.045,CustomColors.customBlack,bold:true),
-                        CustomSizes().heightBox(context,0.02),
-                        text(context, "Deliver or recieve items such as gifts ,documents,keys", 0.035, CustomColors.customGrey),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            CustomSizes().heightBox(context, 0.065),
-            Container(
-              width: CustomSizes().dynamicWidth(context, 0.85),
-              height: CustomSizes().dynamicHeight(context, .15),
-              decoration: BoxDecoration(
-                  color: CustomColors.customWhite,
-                  borderRadius: BorderRadius.circular(
-                    CustomSizes().dynamicWidth(context, 0.05),
-                  )),
-            )
-          ],
-        ),
-      ),
+      body: noActiverOrder(context),
     );
   }
+}
+
+Widget noActiverOrder(context) {
+  return Container(
+    width: CustomSizes().dynamicWidth(context, 1),
+    height: CustomSizes().dynamicHeight(context, 1),
+    color: CustomColors.customGrey.withOpacity(0.2),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        orderCard(
+            context,
+            "https://www.pngkit.com/png/full/72-724560_png-file-tracking-parcel-png.png",
+            "Send Package",
+            "Deliver or recieve items such as gifts ,documents,keys",
+            CustomColors.customYellow),
+        CustomSizes().heightBox(context, 0.065),
+        orderCard(
+            context,
+            "https://cdn0.iconfinder.com/data/icons/line-design-word-processing-set-3-1/21/mailing-recipient-list-512.png",
+            "I am Recipent",
+            "Track an incoming delivery in the app",
+            CustomColors.customBlack)
+      ],
+    ),
+  );
+}
+
+Widget orderCard(context, image, title, subtitle, pngColor) {
+  return Container(
+    width: CustomSizes().dynamicWidth(context, 0.85),
+    height: CustomSizes().dynamicHeight(context, .15),
+    decoration: BoxDecoration(
+      color: CustomColors.customWhite,
+      borderRadius: BorderRadius.circular(
+        CustomSizes().dynamicWidth(context, 0.05),
+      ),
+    ),
+    child: Row(
+      children: [
+        SizedBox(
+          width: CustomSizes().dynamicWidth(context, 0.3),
+          height: CustomSizes().dynamicHeight(context, .1),
+          child: Image.network(
+            image,
+            color: pngColor,
+          ),
+        ),
+        Flexible(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              text(context, title, 0.045, CustomColors.customBlack, bold: true),
+              CustomSizes().heightBox(context, 0.02),
+              text(context, subtitle, 0.035, CustomColors.customGrey),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
 }
--- lib/Khubaib/completed.dart
+++ lib/Khubaib/completed.dart
@@ -2,6 +2,7 @@ import 'package:courierapp/Widgets/text_widget.dart';
 import 'package:courierapp/utils/config.dart';
 import 'package:courierapp/utils/dynamic_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class CompletedOrder extends StatefulWidget {
   const CompletedOrder({Key? key}) : super(key: key);
@@ -14,18 +15,138 @@ class _CompletedOrderState extends State<CompletedOrder> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        width: CustomSizes().dynamicWidth(context, 1),
-        height: CustomSizes().dynamicHeight(context, 1),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            text(
-                context, "Complete order", 0.05, CustomColors.customBlack)
-          ],
-        ),
-      ),
+      body: noCompletedOrder(context),
     );
   }
+}
+
+Widget noCompletedOrder(context) {
+  return SizedBox(
+    width: CustomSizes().dynamicWidth(context, 1),
+    height: CustomSizes().dynamicHeight(context, 1),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Stack(
+          children: [
+            Image.network(
+              "https://cdn.iconscout.com/icon/free/png-256/save-3244517-2701888.png",
+              color: CustomColors.customYellow,
+              width: CustomSizes().dynamicWidth(context, 0.26),
+            ),
+            Positioned(
+              right: 0,
+              child: CircleAvatar(
+                radius: CustomSizes().dynamicHeight(context, 0.021),
+                backgroundColor: CustomColors.customBlack,
+                child: Icon(
+                  Icons.error,
+                  size: CustomSizes().dynamicHeight(context, 0.04),
+                ),
+              ),
+            )
+          ],
+        ),
+        CustomSizes().heightBox(context, 0.03),
+        text(context, "Oooops !", 0.06, CustomColors.customLightBlack,
+            bold: true),
+        CustomSizes().heightBox(context, 0.04),
+        text(context, "No Data Found!", 0.04, CustomColors.customGrey,
+            bold: true),
+      ],
+    ),
+  );
+}
+
+Widget completedOrder(context) {
+  return SizedBox(
+    width: CustomSizes().dynamicWidth(context, 1),
+    height: CustomSizes().dynamicHeight(context, 1),
+    child: ListView.builder(
+      padding: EdgeInsets.symmetric(
+        vertical: CustomSizes().dynamicHeight(context, 0),
+      ),
+      itemCount: 2,
+      shrinkWrap: true,
+      itemBuilder: (BuildContext context, int index) {
+        return completedOrderCard(context);
+      },
+    ),
+  );
+}
+
+Widget completedOrderCard(context) {
+  return Container(
+    width: CustomSizes().dynamicWidth(context, 1),
+    height: CustomSizes().dynamicHeight(context, 0.19),
+    padding: EdgeInsets.symmetric(
+      horizontal: CustomSizes().dynamicWidth(context, 0.05),
+      vertical: CustomSizes().dynamicHeight(context, 0.01),
+    ),
+    margin: EdgeInsets.symmetric(
+      vertical: CustomSizes().dynamicHeight(context, 0.0015),
+    ),
+    color: CustomColors.customWhite,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            text(context, "Completed 14 Decemeber 3:14PM", 0.035,
+                CustomColors.customGrey,
+                bold: true),
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: CustomSizes().dynamicWidth(context, 0.01),
+              ),
+              decoration: BoxDecoration(
+                color: CustomColors.customGrey,
+                borderRadius: BorderRadius.circular(
+                  CustomSizes().dynamicWidth(context, 0.05),
+                ),
+              ),
+              child: text(context, "14325", 0.03, CustomColors.customBlack),
+            )
+          ],
+        ),
+        text(context, "\$370", 0.04, CustomColors.customBlack, bold: true),
+        text(
+            context,
+            "Includes best practices that pros apply, as well as going over common mistakes that many Node.js developers make.",
+            0.03,
+            CustomColors.customLightBlack,
+            bold: true),
+        RatingBar.builder(
+          glow: false,
+          initialRating: 3,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemSize: CustomSizes().dynamicWidth(context, 0.05),
+          unratedColor: CustomColors.customGrey,
+          itemBuilder: (context, _) => const Icon(
+            Icons.star_rounded,
+            color: CustomColors.customYellow,
+          ),
+          onRatingUpdate: (rating) {},
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: CustomSizes().dynamicWidth(context, 0.05),
+              vertical: CustomSizes().dynamicHeight(context, 0)),
+          decoration: BoxDecoration(
+            color: CustomColors.customYellow,
+            borderRadius: BorderRadius.circular(
+              CustomSizes().dynamicWidth(context, 0.05),
+            ),
+          ),
+          child: text(context, "RATE", 0.035, CustomColors.customBlack,bold:true),
+        )
+      ],
+    ),
+  );
 }
