53f560ad58ad7833afea5e40839ab3ec1680a7d5
Hassan Gujjar <hbsgujjar111h@gmail.com>
2022-02-18 11:29:47 +0500

    Hassan ~ Font changes and modifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:courierapp/Screens/tab_bar.dart';
+import 'package:courierapp/utils/config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: CustomColors.primaryColor,
+        textTheme: GoogleFonts.baloo2TextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: const CustomTabBar(),
     );
